ca0bc12a64ceabf4b985ca3680ee530fd3c80b38
Florian Kreuk <fkreuk@cartermario.com>
2023-11-06 20:17:47 -0500

    added auth resolver and typeDefs to Schemas, updated auth.js in utils

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,20 @@
+const typeDefs = `
+type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+}
+
+type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,9 +1,39 @@
-const withAuth = (req, res, next) => {
-    if (!req.user) {
-      return res.status(401).json({ message: 'Unauthorized' });
-    } else {
-      next();
+//Based on the auth from utils in activity 26 of MERN module
+
+const { GraphQLError } = require('graphql');
+const jwt = require('jsonwebtoken');
+
+const secret = 'mysecretssshhhhhhh';
+const expiration = '2h';
+
+module.exports = {
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
     }
-  };
-  
-  module.exports = withAuth;
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    return req;
+  },
+  signToken: function ({ email, username, _id }) {
+    const payload = { email, username, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
