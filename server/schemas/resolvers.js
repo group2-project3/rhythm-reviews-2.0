6208f98c14d937bd9f6fbb2075474b7aa6f6f521
Florian Kreuk <fkreuk@cartermario.com>
2023-11-06 21:15:28 -0500

    updated resolver.js schema

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,8 +3,26 @@ const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        
-    }
+        users: async () => {
+            return User.find().populate('reviews');
+        },
+        user: async (parent, { username }) => {
+            return User.findOne({ username }).populate('reviews');
+        },
+        reviews: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Review.find(params).sort({ createdAt: -1 });
+        },
+        review: async (parent, { reviewId }) => {
+            return Review.findOne({ _id: reviewId});
+        },
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user.id }).populate('reviews');
+            }
+            throw AuthenticationError;
+        },
+    },
 
 Mutation: {
     addUser: async (parent, { username, email, password }) => {
@@ -29,6 +47,38 @@ Mutation: {
 
             return { token, user };
         },
+    },
+    addReview: async (parent, { content }, context) => {
+        if (context.user) {
+            const review = await Review.create({
+                content,
+                reviewUser: context.user.username,
+            });
+
+            await User.findOneAndUpdate(
+                { _id: context.user,_id },
+                { $addToSet: { reviews: review._id} }
+            );
+
+            return review;
+        }
+        throw AuthenticationError;
+        ('You need to be logged in!');
+    },
+    removeReview: async (parent, { reviewId }, context) => {
+        if (context.user) {
+            const review = await Review.findOneAndDelete({
+                _id: reviewId,
+                reviewUser: context.user.username,
+            });
+
+            await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { reviews: review._id } }
+            );
+            return review;
+        }
+        throw AuthenticationError;
     },
 };
 
