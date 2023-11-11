ee0dc520dbf38a7202f979d9ab0c2f1416cbe8ba
Jason <jasontorrealba199@gmail.com>
2023-11-11 09:51:11 -0500

    Fixed footer back to show on each page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,13 +1,4 @@
-import { useLocation } from 'react-router-dom';
-
 const Footer = () => {
-  const location = useLocation();
-  const isHomePage = location.pathname === '/';
-
-  if (!isHomePage) {
-    return null;
-  }
-  
 return  (
     <>
 <div className="flex flex-col items-center justify-between min-h-screen mt-64">
