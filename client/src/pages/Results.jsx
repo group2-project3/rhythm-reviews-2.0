33a102ccd4c513ac2be8b3ce7b3689a48f481d2f
Jason <jasontorrealba199@gmail.com>
2023-11-12 11:57:29 -0500

    Fixed responsive and added white border to search button

--- client/src/pages/Results.jsx
+++ client/src/pages/Results.jsx
@@ -25,10 +25,10 @@ const Results = (props) => {
         <SearchBar />
       </div>
       <div className="mt-48 search-container"> 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mr-2.5">
           {data && data.getAlbumsByArtist && (
             data.getAlbumsByArtist.map((album) => (
-              <div key={album.idAlbum} className="p-4 mb-4 text-white bg-white/30 rounded-xl hover:scale-110 hover:bg-blue-600 hover:bg-opacity-80 album-search-result" onClick={() => handleAlbumClick(album)}>
+              <div key={album.idAlbum} className="p-4 mb-4 text-white bg-white/30 rounded-xl hover:scale-110 hover:bg-blue-600 hover:bg-opacity-80 album-search-result cursor-pointer" onClick={() => handleAlbumClick(album)}>
                 <h3>{album.strArtist}</h3>
                 <div>{album.strAlbum}</div>
                 <div>{album.intYearReleased}</div>
