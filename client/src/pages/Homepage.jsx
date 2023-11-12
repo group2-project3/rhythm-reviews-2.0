01e86b3e6b0027597840e0ccf83de035880fd1c8
Gil Rosario <grosario@alum.quinnipiac.edu>
2023-11-12 11:46:47 -0500

    updated homepage styling, text fade in, increased font

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,23 +1,36 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 import SearchBar from "../components/SearchBar";
+import '../assets/css/style.css';
 
 const Homepage = () => {
-  
+  const [fadeIn, setFadeIn] = useState(false);
+
+  useEffect(() => {
+    setFadeIn(true);
+  }, []);
+
+  const fadeInClass = fadeIn ? "fade-in" : "";
 
   return (
     <>
-      <div className="mt-60 flex flex-col items-center justify-center mt-64">
-        <h1 className="text-2xl text-center text-white text-shadow">
-          Our platform is for music enthusiasts to explore their favorite artists and albums,
-        </h1>
-        <h2 className="text-2xl text-center text-white mb-11 text-shadow">
-          by sharing reviews and engaging in meaningful discussions about their favorite albums.
-        </h2>
-        <SearchBar />
+      <div className="flex flex-col items-center justify-center mt-80">
+        <div className={`text-center ${fadeInClass}`} >
+          <h1 className="text-3xl text-white text-shadow">
+            Our platform is for music enthusiasts to explore their favorite artists and albums,
+          </h1>
+          <h2 className="mb-12 text-3xl text-white text-shadow">
+            by sharing reviews and engaging in meaningful discussions about their favorite albums.
+          </h2>
         </div>
+        <div className="items-center justify-center">
+          <SearchBar />
+        </div>
+      </div>
     </>
   );
 };
 
 export default Homepage;
+
+
+
