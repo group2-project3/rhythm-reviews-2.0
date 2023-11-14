4c5793fbec7583f0770765410bcaa53fca795af9
jenstem <124745888+jenstem@users.noreply.github.com>
2023-11-13 19:41:58 -0500

    Add styling to GoBack.jsx

--- client/src/components/GoBack.jsx
+++ client/src/components/GoBack.jsx
@@ -1,7 +1,7 @@
 const GoBack = () => {
     return (
         <>
-        <p className="mt-5 text-center text-white underline go-back-link hover:text-blue-700 underline-offset-1 [text-shadow:_2px_2px_4px_rgb(0_0_0_/_100%)]">
+        <p className="links">
             <a href="/">
               &lt;&lt; Go Back to Homepage
             </a>
