a233ab624fd451dea7dafdc2043d049e2791c040
Jason <jasontorrealba199@gmail.com>
2023-11-15 16:27:50 -0500

    changed tailwind to css

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -309,10 +309,10 @@ const Profile = () => {
               Password updated successfully!
             </p>
           </div>
-          <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+          <div className="model-update-div">
             <button
               onClick={closePasswordUpdatedModal}
-              className="close-button block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="close-button model-button"
               type="button"
             >
               Close
@@ -333,10 +333,10 @@ const Profile = () => {
               {modalErrorMessage}
             </p>
           </div>
-          <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+          <div className="model-update-div">
             <button
               onClick={closeModal}
-              className="close-button block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="close-button model-button"
               type="button"
             >
               Close
@@ -357,10 +357,10 @@ const Profile = () => {
               {modalSuccessMessage}
             </p>
           </div>
-          <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+          <div className="model-update-div">
             <button
               onClick={closeModal}
-              className="close-button block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="close-button model-button"
               type="button"
             >
               Close
