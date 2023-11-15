19717cb82df13991c65f5d5124db6ef1433ac6af
jenstem <124745888+jenstem@users.noreply.github.com>
2023-11-14 20:20:11 -0500

    Add CSS styling to Album.jsx

--- client/src/pages/Album.jsx
+++ client/src/pages/Album.jsx
@@ -8,6 +8,7 @@ import GoBack from '../components/GoBack';
 import AddReview from '../components/AddReview';
 import EditReview from '../components/EditReview';
 import defaultPic from '../assets/defaultPic.png';
+import './Album.css';
 
 const Album = () => {
   const { idAlbum } = useParams();
@@ -20,7 +21,7 @@ const Album = () => {
     variables: { idAlbum: idAlbum },
   });
 
-  const [isExpanded, setIsExpanded] = useState(false); 
+  const [isExpanded, setIsExpanded] = useState(false);
 
   const [selectedRating, setSelectedRating] = useState(0);
 
@@ -39,39 +40,39 @@ const Album = () => {
 
   return (
     <>
+    <div className="add-margin-top">
       <SearchBar />
+      </div>
 
-
-      
-      <div className="flex flex-col items-center lg:flex-row lg:justify-center">
-        <div className="grid album-mobile" style={{ width: '380px' }}>
-          <h1 className="text-2xl text-center text-white md:ml-auto lg:text-right">{albumData.getAlbumById.strArtist}</h1>
-            <p className="pt-2 text-center text-white md:ml-auto lg:text-right">{albumData.getAlbumById.strAlbum}</p>
-            <p className="pt-2 text-center text-white md:ml-auto lg:text-right">{albumData.getAlbumById.strLabel}</p>
-            <p className="pt-2 text-center text-white md:block md:ml-auto lg:text-right">{albumData.getAlbumById.strStyle}</p>
+      <div className="flex-container">
+        <div className="add-grid album-mobile" style={{ width: '380px' }}>
+          <h1 className="style-album-data top-space">{albumData.getAlbumById.strArtist}</h1>
+            <p className="style-album-data">{albumData.getAlbumById.strAlbum}</p>
+            <p className="style-album-data">{albumData.getAlbumById.strLabel}</p>
+            <p className="style-album-data">{albumData.getAlbumById.strStyle}</p>
         </div>
-        <div className="inline-block w-full h-auto max-w-xs p-4 m-10 text-white rounded bg-white/30 album-details" style={{ width: '380px' }}>
+        <div className="style-album-details album-details">
           <h2>{albumData.getAlbumById.strAlbum}</h2>
           <div>{albumData.getAlbumById.intYearReleased}</div>
           <img
-            className="w-[400px]"
+            className="adjust-img-width"
             src={albumData.getAlbumById.strAlbumThumb !== null ? albumData.getAlbumById.strAlbumThumb : defaultPic}
             alt={`${albumData.getAlbumById.strArtist} - ${albumData.getAlbumById.strAlbum}`}
           />
         </div>
-        <div className="p-4 text-white lg:w-1/3 d-lg-block" style={{ width: '380px' }}>
-          <p>
+        <div className="style-text">
+          <p className="overflow-text">
             {isExpanded
               ? albumData.getAlbumById.strDescriptionEN
               : `${albumData.getAlbumById.strDescriptionEN?.slice(0, 250)}...`}
           </p>
           {!isExpanded && (
-            <button onClick={() => setIsExpanded(true)} className="text-blue-500 hover:underline">
+            <button onClick={() => setIsExpanded(true)} className="read-more-less">
               Read More
             </button>
           )}
           {isExpanded && (
-            <button onClick={() => setIsExpanded(false)} className="text-blue-500 hover:underline">
+            <button onClick={() => setIsExpanded(false)} className="read-more-less">
               Read Less
             </button>
           )}
@@ -86,5 +87,6 @@ const Album = () => {
     </>
   );
 };
+
 
 export default Album;
